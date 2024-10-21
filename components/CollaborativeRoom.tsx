37dec2efbdafd8fb32bc5c10d62a81a9a1632b54
unknown <kiratoka2@gmail.com>
2024-10-21 07:50:48 +0700

    peningkatan UI

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -120,7 +120,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
                     width={24}
                     height={24}
                     onClick={() => setEditing(true)}
-                    className="pointer"
+                    className="cursor-pointer"
                   />
                 )}
 
